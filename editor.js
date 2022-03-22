0782ded8683049be78d24af948fbc51ca63d8a40
Mateus Oliveira Gil <mateus.gill1326@gmail.com>
2022-03-22 18:23:43 -0300

    Criação de botões

--- editor.js
+++ editor.js
@@ -1,39 +1,4 @@
 
-CDEditor = function (textarea){
-    this.textarea = textarea;
-
-    //declarar variaveis dos botoes
-
-    var textareaSource = null;
-    var container = null;
-    var toolbar = [];
-    var iframe = null;
-
-    //document.getElementById("negrito").onclick = function() {buttonN()};
+const meuCabecalho = document.createElement('h1');
+meuCabecalho.textContent = 'Ola mundo!';
     
-     
-            function buttonN(){
-                document.getElementById("clickMe").onclick = function () { alert('hello!'); };
-                //var button = document.getElementById("text").innerHTML = "AAAAAAA";
-                //return button
-            }
-
-    var initIframe = function (container, textareaSource){
-
-        var iframe = document.createElement('iframe');
-        iframe.setAttribute('src', 'about:blank');
-        iframe.setAttribute('contenteditable', 'true');
-        iframe.setAttribute('id', 'CDEditorIframe');
-        iframe.setAttribute('name', 'CDEditorIframe');
-        iframe.classList.add('cd-editor');
-
-        container.appendChild(iframe);
-
-        CDEditorIframe.documnet.body.innerHTML = textareaSource;
-        CDEditroIframe.document.designMode = 'on';
-        CDEditorIframe.document.body.style.margin = 0;
-        CDEditorIframe.document.body.style.wordwrap = 'break';
-
-    };
-
-}
