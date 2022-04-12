5fe7a8d58e15c74d9b4734c0df6419ed8e893c43
Mateus Oliveira Gil <mateus.gill1326@gmail.com>
2022-04-12 15:01:36 -0300

    Concluzçao do projeto
    Foi finalizado o designer do projeto

--- teste.js
+++ teste.js
@@ -8,75 +8,75 @@ let navbar = document.getElementById("nav");
 
 //botao troca a cor para azul
 let btnAzul = document.createElement("button");
-btnAzul.className = "btn btn-outline-success my-1 my-sm-0";
+btnAzul.className = "btn btn-primary";
 btnAzul.innerHTML = "azul";
-btnAzul.setAttribute("id", "btnAzul");
+btnAzul.setAttribute("id", "btn");
 btnAzul.addEventListener("click", azul);
 
 //botao limpa as alteracoes
 let btnClear = document.createElement("button");
-btnClear.className = "btn btn-outline-success my-1 my-sm-0";
+btnClear.className = "btn btn-primary";
 btnClear.innerHTML = "clear";
-btnClear.setAttribute("id", "btnClear");
+btnClear.setAttribute("id", "btn");
 btnClear.addEventListener("click", clear)
 
 //Botao deixa em negrito a letras
 
 let btnNgt = document.createElement("button");
-btnNgt.className = "btn btn-outline-success my-1 my-sm-0";
+btnNgt.className = "btn btn-primary";
 btnNgt.innerHTML = "Negrito";
-btnNgt.setAttribute("id", "btnNgt");
+btnNgt.setAttribute("id", "btn");
 btnNgt.addEventListener("click", negrito);
 
 //botao italico
 
 let btnItc = document.createElement("button");
-btnItc.className = "btn btn-outline-success my-1 my-sm-0";
+btnItc.className = "btn btn-primary";
 btnItc.innerHTML = "Intalico";
-btnItc.setAttribute("id", "btnItc");
+btnItc.setAttribute("id", "btn");
 btnItc.addEventListener("click", italico);  
 
 //Subrinha o texto
 
 let btnSub = document.createElement("button");
-btnSub.className = "btn btn-outline-success my-1 my-sm-0";
+btnSub.className = "btn btn-primary";
 btnSub.innerHTML = "Subrinhar";
-btnSub.setAttribute("id", "btnSub");
+btnSub.setAttribute("id", "btn");
 btnSub.addEventListener("click", Subrinha);  
 
 
 //tacha o texto
 
 let btnTac = document.createElement("button");
-btnTac.className = "btn btn-outline-success my-1 my-sm-0";
+btnTac.className = "btn btn-primary";
 btnTac.innerHTML = "Tachar";
-btnTac.setAttribute("id", "btnSub");
+btnTac.setAttribute("id", "btn");
 btnTac.addEventListener("click", tachar);
 
 //Alinha o texto para o centro
 
 let btnAlct = document.createElement("button");
-btnAlct.className = "btn btn-outline-success my-1 my-sm-0";
+btnAlct.className = "btn btn-primary";
 btnAlct.innerHTML = "Centro";
-btnAlct.setAttribute("id", "btnAlct");
+btnAlct.setAttribute("id", "btn");
 btnAlct.addEventListener("click", AlgCentro);
 
 
 //Alinha o texto para a esquerda
 
 let btnEsq = document.createElement("button");
-btnEsq.className = "btn btn-outline-success my-1 my-sm-0";
+btnEsq.className = "btn btn-primary";
 btnEsq.innerHTML = "Esquerda";
-btnEsq.setAttribute("id", "btnEsq");
+btnEsq.setAttribute("id", "btn");
 btnEsq.addEventListener("click", AlgEsquerda);
 
 
 //Alinha o texto para a direita
 
 let btnDir = document.createElement("button");
-btnDir.className = "btn btn-outline-success my-1 my-sm-0";
+btnDir.className = "btn btn-primary";
 btnDir.innerHTML = "Direita";
-btnDir.setAttribute("id", "btnDir");
+btnDir.setAttribute("id", "btn");
 btnDir.addEventListener("click", AlgDireita);
 
 
@@ -115,7 +115,7 @@ function clear(){
 function azul (){
 
     teste.style.color = "blue";
-   
+    
 };
 
 
